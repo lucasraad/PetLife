38fff78427e60b8fa83bab0c255e9fe1fe2d7991
lucas raad <lucasraadr@gmail.com>
2024-02-06 22:00:42 -0300

    segundo commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var elementosDuvida = document.querySelectorAll('.duvida') //foi criado uma variavel para guarda esta informaçao para ser usada
+//primeira linha esta pegando todos os elementos
+
+elementosDuvida.forEach(function (duvida) { //segunda linha esta querendo executar uma função para cada elemento duvida
+    duvida.addEventListener('click', function () { // terceira linha esta querendo adicionar para cada elemento de duvida, um evento de click
+        duvida.classList.toggle('ativa') //o evento de click ira o fundo verde
+    })
+
+}) //vai ser uma função que vai ser executada para cada elemento que esta na lista
+
